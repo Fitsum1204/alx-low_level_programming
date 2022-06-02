7120b72aee5eccaca8da17b64c90fd4cdab0c5a6
Fitsum1204 <fitsumzione@gmail.com>
2022-06-02 15:38:36 -0700

    puts work

--- 0x00-hello_world/4.puts.c
+++ 0x00-hello_world/4.puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("\*programming is like building a multilingual puzzel");
-     	return (0);
+puts("\*programming is like building a multilingual puzzel");
+return (0);
 }
