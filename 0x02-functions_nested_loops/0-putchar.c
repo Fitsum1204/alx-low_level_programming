36ce94c8fbd58f82a4ce13617a0f21dd0077df2c
Fitsum1204 <fitsumzione@gmail.com>
2022-06-07 05:01:25 -0700

    using -putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+* main - prints _putchar
+* Return: Always 0 (Success)
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+int main(void)
+{
+
+	int i = 0;
+	char str[10]={'_','p','u','t','c','h','a','r','\n'};
+	while (i < 10)
+	{
+	_putchar (str[i]);
+	i++;
+	}	
+
+	return (0);
+}
