973e636578b03ba77a0277b69b5633aade662d69
Fitsum1204 <fitsumzione@gmail.com>
2022-06-08 13:12:13 -0700

    print lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,16 +6,13 @@
 */
 
 void print_alphabet(void)
-{
-	
-	char alpha = 'a';
-	
+{	
+	char alpha = 'a';	
 	while (alpha <= 'z')
-        {
+	{
 		_putchar(alpha);
 		alpha++;
-        }
+	}
 	_putchar('\n');
-
 }
 
