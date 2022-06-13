0ae56272ed38cc0096f6993b1cc29c6c525e63bb
Fitsum1204 <fitsumzione@gmail.com>
2022-06-13 12:58:53 -0700

    print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
  * print_rev - prints a string, in reverse, followed by a new line
- *  @s: string to reverse
+ * @s: string to reverse
  */
+
 void print_rev(char *s)
 {
 
