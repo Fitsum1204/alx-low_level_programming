0c3ad43f6fa3a94486f695df0800f49c7da2bfea
Fitsum1204 <fitsumzione@gmail.com>
2022-06-13 04:00:05 -0700

    swap integer

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * swap_int -> function that swaps the values of two integers
- * @a:  pointer point to int a 
- * @b;  pointer point to int b
- *
+ * @a:  pointer point to int a
+ * @b:  pointer point to int b
  */
 void swap_int(int *a, int *b)
 {
