ab64642e3a08a62304ee6c7ebc24d7a1d9405144
Fitsum1204 <fitsumzione@gmail.com>
2022-06-13 05:28:26 -0700

    count length of  length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,15 +3,16 @@
 /**
  * _strlen -> count number of character in a string
  * @s: point to string
+ * return: length
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		i++;
 		s++;
 	}
-	return i;	
+	return (i);	
 }
