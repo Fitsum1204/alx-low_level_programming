0b91dcf786995f5dbc59b82f99ac0b2d211f81e8
Fitsum1204 <fitsumzione@gmail.com>
2022-06-16 01:53:07 -0700

    concat 2 string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * *_strncat - concat 2 string useing n bytes
+ * _strncat - concat 2 string useing n bytes
  * @dest: char
  * @src: char
  * @n:length int
- * return: char
+ * Return:char
  */
 char *_strncat(char *dest, char *src, int n)
 {
