6e9660c311a216d0c8ef19a4e6e27ada99daa985
Fitsum1204 <fitsumzione@gmail.com>
2022-06-16 01:44:50 -0700

    concat 2 string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,17 +8,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	char *s = dest;
 	int i;
 
 	while (*dest != '\0')
 	{
 		dest++;
 	}
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		*dest = *(src + i);
 		dest++;
 	}
 	*dest = '\0';
-	return (dest);
+	return (s);
 }
