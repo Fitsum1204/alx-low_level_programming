06813641d710d173cfdfff065ff6d4a2c6451be2
Fitsum1204 <fitsumzione@gmail.com>
2022-06-16 02:24:21 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: char
+ * @src: char
+ * @n: intiger
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *s = dest;
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		*dest = src[i];
+		*dest++;
+	}
+	*dest = '\0';
+	return (s);
+}
