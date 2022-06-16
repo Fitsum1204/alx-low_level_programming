8867c308df37e493cfd4b37be685996ff6081814
Fitsum1204 <fitsumzione@gmail.com>
2022-06-15 23:14:07 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,16 +3,16 @@
  *
  *
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	
 	while (*dest != '\0')
-		dest++
-	dest++
+		dest++;
+	dest++;
 	for (int i= 0; *src != '\0';i++)
 	{
-		dest + i = *src;
+		*(dest + i) = *src;
 			src++;
 	}
 	
